a949a930df70bcc5a6e41453c98c38364593cfaf
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-04-19 02:00:01 +0200

    added antd form

--- studytrack/src/App.js
+++ studytrack/src/App.js
@@ -1,84 +1,21 @@
-import ReactDOM from 'react-dom';
-import React, { useState } from 'react';
-import { Form, Input, Button, Radio } from 'antd';
+//import ReactDOM from 'react-dom';
+//import React, { useState } from 'react';
+import HorizontalLoginForm from "./components/AntForm.js";
 
 import './App.css';
 import 'antd/dist/antd.css';
 
-import FormTest from"./components/FormTest";
+
+import FormTest from "./components/FormTest";
 
 function App() {
   return (
     <>
       <FormTest/>
-      <FormLayoutDemo />
+      
+      <HorizontalLoginForm/>
     </>
   );
 }
 
 export default App;
-
-
-
-
-const FormLayoutDemo = () => {
-  const [form] = Form.useForm();
-  const [formLayout, setFormLayout] = useState('horizontal');
-
-  const onFormLayoutChange = ({ layout }) => {
-    setFormLayout(layout);
-  };
-
-  const formItemLayout =
-    formLayout === 'horizontal'
-      ? {
-          labelCol: {
-            span: 4,
-          },
-          wrapperCol: {
-            span: 14,
-          },
-        }
-      : null;
-  const buttonItemLayout =
-    formLayout === 'horizontal'
-      ? {
-          wrapperCol: {
-            span: 14,
-            offset: 4,
-          },
-        }
-      : null;
-  return (
-    <>
-      <Form
-        {...formItemLayout}
-        layout={formLayout}
-        form={form}
-        initialValues={{
-          layout: formLayout,
-        }}
-        onValuesChange={onFormLayoutChange}
-      >
-        <Form.Item label="Form Layout" name="layout">
-          <Radio.Group value={formLayout}>
-            <Radio.Button value="horizontal">Horizontal</Radio.Button>
-            <Radio.Button value="vertical">Vertical</Radio.Button>
-            <Radio.Button value="inline">Inline</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item label="Field A">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item label="Field B">
-          <Input placeholder="input placeholder" />
-        </Form.Item>
-        <Form.Item {...buttonItemLayout}>
-          <Button type="primary">Submit</Button>
-        </Form.Item>
-      </Form>
-    </>
-  );
-};
-
-//ReactDOM.render(<FormLayoutDemo />, mountNode);
