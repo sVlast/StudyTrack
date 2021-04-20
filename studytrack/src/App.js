5cfbe3d519ef711f4fdb5da2af519a9d08ff57cd
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-04-20 16:52:02 +0200

    updated packages

--- studytrack/src/App.js
+++ studytrack/src/App.js
@@ -1,12 +1,14 @@
 //import ReactDOM from 'react-dom';
 //import React, { useState } from 'react';
-import HorizontalLoginForm from "./components/AntForm.js";
+//import HorizontalLoginForm from "./components/AntForm.js";
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './App.css';
 import 'antd/dist/antd.css';
 
-
-import FormTest from "./components/FormTest";
+//import FormTest from "./components/FormTest";
 import LogInModal from "./components/LogInModal.js";
 
 function App() {
