34efc2bfcbc7fc88e7d57974d8ff22f189d49227
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-04-22 13:44:41 +0200

    yarn rebuild

--- studytrack/src/App.js
+++ studytrack/src/App.js
@@ -11,15 +11,6 @@ import 'antd/dist/antd.css';
 //let database = firebase.database();
 
 
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> a06a2c65cbaf2946429c7e959905bdcf25617164
->>>>>>> db8d2e5426af942940cbf593f75ccaceb2f68929
-
 function App() {
   return (
     <>
