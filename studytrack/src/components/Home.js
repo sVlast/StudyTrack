5bd9c6836b6c60ccffe3f2581890fcb6921fa760
tomislavkatalenic <tomislavkatalenic@users.noreply.github.com>
2021-04-27 00:07:37 +0200

    Added Home component for the landing page

---
+++ studytrack/src/components/Home.js
@@ -0,0 +1,13 @@
+import LogInModal from "./components/LogInModal.js";
+import RegisterModal from "./components/RegisterModal.js";
+
+function Home() {
+    return (
+        <>
+            <LogInModal />
+            <RegisterModal />
+        </>
+    );
+}
+
+export default Home;
