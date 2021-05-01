2a108fb2f10d0f5a0484e198e8fccb6b2759763a
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-05-01 20:47:56 +0200

    added routing (testing components)

--- studytrack/src/App.js
+++ studytrack/src/App.js
@@ -1,21 +1,33 @@
 import firebase from './util/firebase';
 import './App.css';
 import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 //Test components
 import Basicform from "./components/Basicform";
 //Working components
-import Dashboard from './components/Dashboard.js';
 import Dashboard from './components/Dashboard/Dashboard.js';
 import LandingPage from './components/Landing.js';
 
 
 function App() {
   return (
-    <>
-      <LandingPage/>
-      <Dashboard/>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Link to="/Dashboard">Link</Link>
+          <LandingPage />
+        </Route>
+        <Route path="/Dashboard">
+          {/*
+          test route
+          */}
+          <Link to="/">Link</Link>
+          <Dashboard />
+        </Route>
+
+      </Switch>
+    </Router>
   );
 }
 
