4cc479ab9e26f61715b33d90b385781a107ffbf4
tomislavkatalenic <tomislavkatalenic@users.noreply.github.com>
2021-05-02 19:13:46 +0200

    added header component and styled whole landing

---
+++ studytrack/src/components/Landing/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+    return (
+        <>
+            <div className="header">
+                <img src="/images/ST_Logo.png"></img>
+            </div>
+        </>
+    );
+}
+
+export default Header;
--- studytrack/src/components/Landing/Landing.js
+++ studytrack/src/components/Landing/Landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner.js';
 import BlueContent from './BlueContent.js';
+import Header from './Header.js';
 import LogInModal from "./LogInModal.js";
 import RegisterModal from "./RegisterModal.js";
 
@@ -9,6 +10,7 @@ function LandingPage() {
         <>
             <LogInModal />
             <RegisterModal />
+            <Header/>
             <Banner/>
             <BlueContent/>
         </>
