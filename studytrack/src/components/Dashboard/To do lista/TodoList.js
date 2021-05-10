783288bfb2c071bfa31744ff871c47e4f9458781
Mihael Babić <72862332+mbabic5@users.noreply.github.com>
2021-05-10 19:36:14 +0200

    changed read data once in TodoList to listen for events

--- studytrack/src/components/Dashboard/To do lista/TodoList.js
+++ studytrack/src/components/Dashboard/To do lista/TodoList.js
@@ -5,85 +5,75 @@ import TodoForm from "./TodoForm";
 import "./TodoList.css";
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
-    const taskRef = firebase.database().ref("Task");
+  const [todos, setTodos] = useState([]);
+  const taskRef = firebase.database().ref("Task");
 
+  useEffect(() => {
+    taskRef.on("value", (snapshot) => {
+      const tasks = snapshot.val();
+      const taskList = [];
+      for (let id in tasks) {
+        taskList.push(tasks[id]);
+      }
+      setTodos(taskList);
+      console.log(tasks);
+      console.log(taskList);
+    });
 
-    useEffect(() => {
-        taskRef
-            .get()
-            .then((snapshot) => {
-                if (snapshot.exists()) {
-                    console.log(snapshot);
-                    const tasks = snapshot.val();
-                    const taskList = [];
-                    for (let id in tasks) {
-                        taskList.push(tasks[id]);
-                    }
-                    setTodos(taskList);
-                    console.log(tasks);
-                    console.log(taskList);
-                } else {
-                    console.log("No data available");
-                }
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, []);
+    return() => taskRef.off();
+  }, []);
 
+  const addTodo = (todo) => {
+    if (!todo.title || /^\s*$/.test(todo.title)) {
+      return;
+    }
 
-    const addTodo = (todo) => {
-        if (!todo.title || /^\s*$/.test(todo.title)) {
-            return;
-        }
+    const newTodos = [todo, ...todos];
 
-        const newTodos = [todo, ...todos];
+    setTodos(newTodos);
 
-        setTodos(newTodos);
+    //firebase write
+    taskRef.push(todo);
+  };
 
-        //firebase write
-        taskRef.push(todo);
-    };
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.title || /^\s*$/.test(newValue.title)) {
+      return;
+    }
 
-    const updateTodo = (todoId, newValue) => {
-        if (!newValue.title || /^\s*$/.test(newValue.title)) {
-            return;
-        }
-
-        setTodos((prev) =>
-            prev.map((item) => (item.id === todoId ? newValue : item))
-        );
-    };
-
-    const removeTodo = (id) => {
-        const removeArr = [...todos].filter((todo) => todo.id !== id);
-
-        setTodos(removeArr);
-    };
-
-    const completeTodo = (id) => {
-        let updatedTodos = todos.map((todo) => {
-            if (todo.id === id) {
-                todo.complete = !todo.complete;
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    };
-
-    return (
-        <div className="todolist">
-            <h2>Add a task</h2>
-            <TodoForm onSubmit={addTodo} />
-            <Todo
-                todos={todos}
-                completeTodo={completeTodo}
-                removeTodo={removeTodo}
-                updateTodo={updateTodo}
-            />
-        </div>
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
     );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todolist">
+      <h2>Add a task</h2>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
 }
 
 export default TodoList;
