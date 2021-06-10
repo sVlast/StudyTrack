e8062144a4626617dbae459f9c8a07104f3ed10c
Sandro Vlastelica <sandro.vlastelica@hotmail.com>
2021-06-10 13:31:32 +0200

    Samo hardkoo

--- studytrack/src/components/Dashboard/Profil/Profil.js
+++ studytrack/src/components/Dashboard/Profil/Profil.js
@@ -1,15 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import PasswordForm from './PasswordForm.js';
-import UsernameForm from './UsernameForm.js';
+import React, { useState, useEffect } from "react";
+import PasswordForm from "./PasswordForm.js";
+import UsernameForm from "./UsernameForm.js";
 import { List, Card } from "antd";
-import './Profil.css';
-import moment from 'moment';
-import { useDatabaseContext } from '../../../contexts/DatabaseContext.js';
+import "./Profil.css";
+import moment from "moment";
+import { useDatabaseContext } from "../../../contexts/DatabaseContext.js";
 import firebase from "../../../util/firebase.js";
 
-
 function Profil() {
-    const [todos, setTodos] = useState([]);
+    const [pointCollection, setPointCollection] = useState([]);
     const [time, setTime] = useState(moment().format("DD/MM/YYYY"));
     const [taskID, setTaskID] = useState("");
     const userID = useDatabaseContext();
@@ -19,16 +18,151 @@ function Profil() {
         taskRef.on("value", (snapshot) => {
             const tasks = snapshot.val();
             const taskList = [];
+            const temp = [];
             for (let id in tasks) {
-                taskList.unshift({ id, ...tasks[id] });
+                switch (tasks[id].title) {
+                    case "Grafički dizajn":
+                        if (temp["Grafički dizajn"] === undefined) {
+                            temp["Grafički dizajn"] = {
+                                title: "Grafički dizajn",
+                                class: 0,
+                                lab: 0,
+                                classMax: 0,
+                                labMax: 0,
+                            };
+                        }
+
+                        if (tasks[id].type == "Predavanja") {
+                            temp["Grafički dizajn"].classMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["Grafički dizajn"].class += 1;
+                            }
+                        } else if (tasks[id].type == "Laboratorijske vježbe") {
+                            temp["Grafički dizajn"].labMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["Grafički dizajn"].lab += 1;
+                            }
+                        } else {
+                            console.error("Error!");
+                        }
+                        break;
+                    case "Digitalna fotografija":
+                        if (temp["Digitalna fotografija"] === undefined) {
+                            temp["Digitalna fotografija"] = {
+                                title: "Digitalna fotografija",
+                                class: 0,
+                                lab: 0,
+                                classMax: 0,
+                                labMax: 0,
+                            };
+                        }
+
+                        if (tasks[id].type == "Predavanja") {
+                            temp["Digitalna fotografija"].classMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["Digitalna fotografija"].class += 1;
+                            }
+                        } else if (tasks[id].type == "Laboratorijske vježbe") {
+                            temp["Digitalna fotografija"].labMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["Digitalna fotografija"].lab += 1;
+                            }
+                        } else {
+                            console.error("Error!");
+                        }
+                        break;
+                    case "Poslovni engleski jezik za IT":
+                        if (
+                            temp["Poslovni engleski jezik za IT"] === undefined
+                        ) {
+                            temp["Poslovni engleski jezik za IT"] = {
+                                title: "Poslovni engleski jezik za IT",
+                                class: 0,
+                                lab: 0,
+                                classMax: 0,
+                                labMax: 0,
+                            };
+                        }
+
+                        if (tasks[id].type == "Predavanja") {
+                            temp["Poslovni engleski jezik za IT"].classMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp[
+                                    "Poslovni engleski jezik za IT"
+                                ].class += 1;
+                            }
+                        } else if (tasks[id].type == "Laboratorijske vježbe") {
+                            temp["Poslovni engleski jezik za IT"].labMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["Poslovni engleski jezik za IT"].lab += 1;
+                            }
+                        } else {
+                            console.error("Error!");
+                        }
+                        break;
+                    case "XML programiranje":
+                        if (temp["XML programiranje"] === undefined) {
+                            temp["XML programiranje"] = {
+                                title: "XML programiranje",
+                                class: 0,
+                                lab: 0,
+                                classMax: 0,
+                                labMax: 0,
+                            };
+                        }
+
+                        if (tasks[id].type == "Predavanja") {
+                            temp["XML programiranje"].classMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["XML programiranje"].class += 1;
+                            }
+                        } else if (tasks[id].type == "Laboratorijske vježbe") {
+                            temp["XML programiranje"].labMax += 1;
+                            if (
+                                tasks[id].wasPresent &&
+                                tasks[id].comment != ""
+                            ) {
+                                temp["XML programiranje"].lab += 1;
+                            }
+                        } else {
+                            console.error("Error!");
+                        }
+                        break;
+                }
+                //taskList.unshift(temp);
             }
-            setTodos(taskList);
+            //console.log(temp);
+            for (let i in temp) {
+                taskList.push(temp[i]);
+            }
+            setPointCollection(taskList);
             //console.log(tasks);
             //console.log(taskList);
         });
         return () => taskRef.off();
     }, []);
-    
+
     return (
         <div className="profil-parent">
             <div className="profil-content">
@@ -45,26 +179,29 @@ function Profil() {
                 <h2>Your points:</h2>
                 <List
                     grid={{ gutter: 16, column: 3 }}
-                    dataSource={todos}
+                    dataSource={pointCollection}
                     renderItem={(item) => (
                         <List.Item>
                             <Card title={item.title}>
                                 <span>
-                                    <span>{item.type}</span>
-                                    <span>3</span>
+                                    <span>Predavanja</span>
+                                    <span>
+                                        {item.class}/{item.classMax}
+                                    </span>
                                 </span>
                                 <span>
-                                    <span>{item.type}</span>
-                                    <span>4</span>
+                                    <span>Laboratorijske Vježbe</span>
+                                    <span>
+                                        {item.lab}/{item.labMax}
+                                    </span>
                                 </span>
-
                             </Card>
                         </List.Item>
                     )}
                 />
             </div>
         </div>
-    )
-};
+    );
+}
 
 export default Profil;
